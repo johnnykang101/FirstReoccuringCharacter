5a38d37b2aab9586865d1b0bb4c7cd5fc5dad316
johnnykang101 <56775858+johnnykang101@users.noreply.github.com>
2020-07-07 21:53:27 -0700

    Test Function
    Created a PowerShell Function that return the first reoccurring character in a string

---
+++ TestFunction.ps1
@@ -0,0 +1,47 @@
+﻿<#
+    .SYNOPSIS
+        Test Interview: to call a function to return first reocurring character
+    .EXAMPLE
+        .\TestFunction.ps1 -foo "abcdedcba"
+#>
+Param
+(
+    [parameter(Mandatory=$true)]
+    [string]
+    $foo 
+)
+
+
+function Get-FirstReoccuringChar {
+    <#
+        .SYNOPSIS
+            Return first reoccuring character
+        .EXAMPLE
+            Get-FirstReoccuringChar -strValue "abcdedcba"
+    #>
+    Param
+    (
+        [parameter(Mandatory=$true)]
+        [string]
+        $strValue
+    )
+
+    # Create a new Array
+    $arr = New-Object System.Collections.ArrayList
+
+    for($i=0; $i -le $strValue.Length - 1; $i++)
+    {
+        $c = $strValue[$i]
+
+        if ($arr.Contains($c))
+        {
+            return $c
+        }
+        else
+        {
+            $arr.Add($c) | Out-Null
+        }
+    }
+}
+
+Get-FirstReoccuringChar -strValue $foo
